46f367f5ac694b85f3015166209b43f1b9d542e2
kenzo6c <kenzo.carneiro.36@gmail.com>
2022-06-21 00:00:59 +0200

    Documented Vectors

--- vector.lua
+++ vector.lua
@@ -1,5 +1,9 @@
+--- @class Vector
 Vector = {x = 0; y = 0}
 
+--- Constructor of Vector
+--- @param x number
+--- @param y number
 function Vector:new(x, y)
     local e = {}
     setmetatable(e, self)
@@ -9,22 +13,32 @@ function Vector:new(x, y)
     return e
 end
 
+--- String conversion for Vector
+--- @return string
 function Vector:__tostring()
     return "Vector(" .. self.x .. ", " .. self.y .. ")"
 end
 
+--- Norm of the Vector
+--- @return number
 function Vector:__len()
     return math.sqrt(self.x^2 + self.y^2)
 end
 
+--- Add to another vector
+--- @return Vector
 function Vector:__add(v)
     return Vector:new(self.x + v.x, self.y + v.y)
 end
 
+--- Subtract from another vector
+--- @return Vector
 function Vector:__sub(v)
     return Vector:new(self.x - v.x, self.y - v.y)
 end
 
+--- Multiply with a scalar or a vector
+--- @return Vector|number
 function Vector:__mul(v)
     if type(v) == "number" then
         return Vector:new(self.x * v, self.y * v)
@@ -33,26 +47,40 @@ function Vector:__mul(v)
     end
 end
 
+--- Divide with a scalar
+--- @param a number
+--- @return Vector
 function Vector:__div(a)
     return Vector:new(self.x / a, self.y / a)
 end
 
-function Vector:__eq(a)
-    return self.x == a.x and self.y == a.y
+--- Test for equality with another vector
+--- @param v Vector
+--- @return boolean
+function Vector:__eq(v)
+    return self.x == v.x and self.y == v.y
 end
 
-function Vector:__unm(a)
+--- Negative of the Vector
+--- @return Vector
+function Vector:__unm()
     return Vector:new(-self.x, -self.y)
 end
 
+--- Normalize the Vector
+--- @return Vector
 function Vector:normalized()
     return Vector:new(self.x, self.y) / self:__len()
 end
 
+--- Floor the Vector
+--- @return Vector
 function Vector:floor()
     return Vector:new(math.floor(self.x), math.floor(self.y))
 end
 
+--- Round the Vector
+--- @return Vector
 function Vector:round()
     return Vector:new(math.floor(self.x + 0.5), math.floor(self.y + 0.5))
 end
