685ddcf9631fe807a49c8695cb1b422467ad7e03
Nicolas Bédrunes <62482340+nicos-code@users.noreply.github.com>
2022-06-21 14:15:12 +0200

    test héritage pour le HUD

---
+++ tests/HudHeritage/main_classWithInheritance.lua
@@ -0,0 +1,24 @@
+if arg[#arg] == "vsc_debug" then require("lldebugger").start() end
+
+--bof
+local classWithInheritance = require("classWithInheritance") 
+--mieux
+local Element = require("element")
+local Panel = require("panel")
+
+local element = classWithInheritance.newElement(100, 200)
+local panel = classWithInheritance.newPanel(200, 300, 300, 400)
+
+element:draw()
+panel:draw()
+print("\n")
+
+local element2 = Element:new(100, 200)
+local panel2 = Panel:new(200, 300, 300, 400)
+
+element2:draw()
+panel2:draw()
+print("\n")
+
+panel2:add(10)
+panel2:draw()
