374a3e3268802ca1a88c30e1bc38c9a6d551656d
kenzo6c <kenzo.carneiro.36@gmail.com>
2022-06-21 22:52:54 +0200

    Implemented Tiled Map & Map Collisions

--- sprite/sprite.lua
+++ sprite/sprite.lua
@@ -8,6 +8,7 @@
 --- @field height number
 --- @field middle Vector
 --- @field frames love.Quad[]|nil
+--- @field twoDFrames boolean
 Sprite = {}
 
 
@@ -17,18 +18,29 @@ Sprite = {}
 --- @param state string
 --- @param width number
 --- @param height number
+--- @param twoDFrames boolean|nil
 --- @return Sprite s sprite table created on the fly.
-function Sprite:new(img, isSheet, state, width, height, middle)
+function Sprite:new(img, isSheet, state, width, height, middle, twoDFrames)
     -- Sprites are created on the fly
-    local s = {loveImg = love.graphics.newImage(img), isSheet=isSheet, state=state, width=width, height=height, middle=middle or Vector:new(width/2, height/2)}
+    local s = {loveImg = love.graphics.newImage(img), isSheet=isSheet, state=state, width=width, height=height, middle=middle or Vector:new(width/2, height/2), twoDFrames=twoDFrames}
     setmetatable(s, self)
     self.__index = self
-
     if isSheet then
-        s.frames = {}
-        for y = 0, s.loveImg:getHeight() - height, height do
-            for x = 0, s.loveImg:getWidth() - width, width do
-                s.frames[#s.frames + 1] = love.graphics.newQuad(x, y, width, height, s.loveImg:getDimensions())
+        if twoDFrames then
+            s.frames = {}
+            for y = 0, s.loveImg:getHeight()/height do
+                s.frames[y] = {}
+                for x = 0, s.loveImg:getWidth()/width do
+                    print(x, y)
+                    s.frames[y][x] = love.graphics.newQuad(x*height, y*width, width, height, s.loveImg:getDimensions())
+                end
+            end
+        else
+            s.frames = {}
+            for y = 0, s.loveImg:getHeight() - height, height do
+                for x = 0, s.loveImg:getWidth() - width, width do
+                    s.frames[#s.frames + 1] = love.graphics.newQuad(x, y, width, height, s.loveImg:getDimensions())
+                end
             end
         end
     end
