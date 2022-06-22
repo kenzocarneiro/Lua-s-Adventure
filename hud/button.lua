a5b0fd8a91d8ecc08f5bc7063fdfa0ed8c0dd13b
Nicolas Bédrunes <62482340+nicos-code@users.noreply.github.com>
2022-06-22 15:46:06 +0200

    Fix somes bugs of HUD Clean main.lua

--- hud/button.lua
+++ hud/button.lua
@@ -1,5 +1,5 @@
 local Panel = require("hud/panel")
-local Text = require("hud/panel")
+local Text = require("hud/text")
 
 local Button = {}
 
@@ -26,61 +26,58 @@ function Button:new(pX, pY, pW, pH, pText, pFont, pColor)
 end
 
 function Button:setImages(pImageDefault, pImageHover, pImagePressed)
-  self.imgDefault = pImageDefault
-  self.imgHover = pImageHover
-  self.imgPressed = pImagePressed
-  self.w = pImageDefault:getWidth()
-  self.h = pImageDefault:getHeight()
+    self.imgDefault = pImageDefault
+    self.imgHover = pImageHover
+    self.imgPressed = pImagePressed
+    self.w = pImageDefault:getWidth()
+    self.h = pImageDefault:getHeight()
 end
 
 function Button:update(dt)
 
-  Panel.update(self,dt)
-  if self.isHover and love.mouse.isDown(1) and
-      self.isPressed == false and
-      self.oldButtonState == false then
-    self.isPressed = true
-    if self.lstEvents["pressed"] ~= nil then
-      self.lstEvents["pressed"]("begin")
+    Panel.update(self,dt)
+    if self.isHover and love.mouse.isDown(1) and self.isPressed == false and self.oldButtonState == false then
+        self.isPressed = true
+        if self.lstEvents["pressed"] ~= nil then
+            self.lstEvents["pressed"]("begin")
+        end
+    else
+        if self.isPressed == true and love.mouse.isDown(1) == false then
+            self.isPressed = false
+            if self.lstEvents["pressed"] ~= nil then
+                self.lstEvents["pressed"]("end")
+            end
+        end
     end
-  else
-    if self.isPressed == true and love.mouse.isDown(1) == false then
-      self.isPressed = false
-      if self.lstEvents["pressed"] ~= nil then
-        self.lstEvents["pressed"]("end")
-      end
-    end
-  end
-  self.oldButtonState = love.mouse.isDown(1)
+    self.oldButtonState = love.mouse.isDown(1)
 end
 
 function Button:draw()
-  love.graphics.setColor(1,1,1)
-  if self.isPressed then
-    if self.imgPressed == nil then
-      Panel.draw(self)
-      love.graphics.setColor(1,1,1,.2)
-      love.graphics.rectangle("fill", self.X, self.Y, self.W, self.H)
+    love.graphics.setColor(1,1,1)
+    if self.isPressed then
+        if self.imgPressed == nil then
+            Panel.draw(self)
+            love.graphics.setColor(1,1,1,.2)
+            love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
+        else
+            love.graphics.draw(self.imgPressed, self.x, self.y)
+        end
+    elseif self.isHover then
+        if self.imgHover == nil then
+            Panel.draw(self)
+            love.graphics.setColor(1,1,1)
+            love.graphics.rectangle("line", self.x+2, self.y+2, self.w-4, self.h-4)
+        else
+            love.graphics.draw(self.imgHover, self.x, self.y)
+        end
     else
-      love.graphics.draw(self.imgPressed, self.X, self.Y)
+        if self.imgDefault == nil then
+            Panel.draw(self)
+        else
+            love.graphics.draw(self.imgDefault, self.x, self.y)
+        end
     end
-  elseif self.isHover then
-    if self.imgHover == nil then
-      Panel.draw(self)
-      love.graphics.setColor(1,1,1)
-      love.graphics.rectangle("line", self.X+2, self.Y+2, self.W-4, self.H-4)
-    else
-      love.graphics.draw(self.imgHover, self.X, self.Y)
-    end
-  else
-    if self.imgDefault == nil then
-      Panel.draw(self)
-    else
-      love.graphics.draw(self.imgDefault, self.X, self.Y)
-    end    
-  end
-  self.Label:draw()
+    self.label:draw()
 end
-  
-return Button
 
+return Button
