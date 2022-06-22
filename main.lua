e7a478d4894650774bf8d83d2bc823c3f09489b5
Tristan <tristanclaude18@gmail.com>
2022-06-22 17:33:21 +0200

    skills + inventory_slots

--- main.lua
+++ main.lua
@@ -20,7 +20,6 @@ function onPanelHover(pState)
 
 local mainFont = love.graphics.newFont("sprites/hud/kenvector_future_thin.ttf", 15)
 love.graphics.setFont(mainFont)
-local Checkbox = require("hud/checkbox")
 
 function love.load()
     love.graphics.setBackgroundColor(55, 55, 55)
@@ -34,10 +33,6 @@ function love.load()
     G_healthHeart = Panel:new(0, 0)
     G_healthHeart:setImage(love.graphics.newImage("sprites/hud/health.png"), 0.1)
 
-    G_panelTest1 = Panel:new(10, 50)
-    G_panelTest1:setImage(love.graphics.newImage("sprites/hud/panel1.png"), 0.1)
-    G_title1 = Text:new(G_panelTest1.x + 35, G_panelTest1.y + 45, 0, 0,
-    "Shield Generator", mainFont, "", "", {157, 164, 174})
     G_healthBar = Bar:new(10,2, 41, 7, 10, nil,{0,255,0})
 
    -- G_button = Button:new(55, 100, 120, 80,"No images", mainFont, {250, 250, 250})
@@ -48,94 +43,86 @@ function love.load()
   largeur = love.graphics.getWidth()
   hauteur = love.graphics.getHeight()
   
-  buttonYes = myGUI.newButton(10, 20, 100, 43,"Yes", mainFont, {151, 220, 250})
-  buttonYes:setImages(
-    love.graphics.newImage("sprites/hud/button_default.png"),
-    love.graphics.newImage("sprites/hud/button_hover.png"),
-    love.graphics.newImage("sprites/hud/button_pressed.png")
+  skill_1 = myGUI.newButton(0, hauteur - 64, 40, 40,"", mainFont, {151, 220, 250})
+  skill_1:setImages(
+    love.graphics.newImage("sprites/hud/health_potion.png")
+   
     )
+    skill_1:setEvent("")
 
-    buttonParam = myGUI.newButton(largeur - 64, hauteur - 64, 16, 16,"", mainFont, {151, 220, 250})
+    buttonParam = myGUI.newButton(largeur -60 , hauteur - 70, 16, 16,"", mainFont, {255, 0, 0})
     buttonParam:setImages(
       love.graphics.newImage("sprites/hud/gear2.jpg")
+   
+
       )
 
-  buttonNo = myGUI.newButton(115, 20, 100, 43,"No", mainFont, {151, 220, 250})
-  buttonNo:setImages(
-    love.graphics.newImage("sprites/hud/button_default.png"),
-    love.graphics.newImage("sprites/hud/button_hover.png"),
-    love.graphics.newImage("sprites/hud/button_pressed.png")
+  skill_2 = myGUI.newButton(65, hauteur - 64, 40, 40,"", mainFont, {0, 255, 0})
+  skill_2:setImages(
+    love.graphics.newImage("sprites/hud/skilll_1.png")
     )
+
+    skill_3 = myGUI.newButton(130, hauteur - 65, 40, 40,"", mainFont, {0, 255, 0})
+  skill_3:setImages(
+    love.graphics.newImage("sprites/hud/boom.png")
+    )
+  
+    offset = largeur / 2
+    distanceBetweenInvSlot = 65
+
+    inventory_slot_1 = myGUI.newButton(offset - distanceBetweenInvSlot * 2, hauteur - 64, 40, 40,"", mainFont, {0, 255, 0})
+    inventory_slot_1:setImages(
+    love.graphics.newImage("sprites/hud/blank_slot.png")
+    )
+    inventory_slot_2 = myGUI.newButton(offset - distanceBetweenInvSlot, hauteur - 64, 40, 40,"", mainFont, {0, 255, 0})
+    inventory_slot_2:setImages(
+    love.graphics.newImage("sprites/hud/blank_slot.png")
+    )
+    inventory_slot_3 = myGUI.newButton(offset, hauteur - 64, 40, 40,"", mainFont, {0, 255, 0})
+    inventory_slot_3:setImages(
+    love.graphics.newImage("sprites/hud/blank_slot.png")
+    )
+    inventory_slot_4 = myGUI.newButton(offset +distanceBetweenInvSlot, hauteur - 64, 40, 40,"", mainFont, {0, 255, 0})
+  inventory_slot_4:setImages(
+    love.graphics.newImage("sprites/hud/blank_slot.png")
+    )
+    inventory_slot_5 = myGUI.newButton(offset +distanceBetweenInvSlot *2, hauteur - 64, 40, 40,"", mainFont, {0, 255, 0})
+    inventory_slot_5:setImages(
+      love.graphics.newImage("sprites/hud/blank_slot.png")
+      )
+
+      title1 = myGUI.newText(skill_1.W/2 , hauteur - skill_1.H -20, 0, 0,
+                           "A", mainFont, "", "", {25, 150, 25})
+  
+      title2 = myGUI.newText(skill_1.W/2 + skill_2.W , hauteur - skill_1.H -20, 0, 0,
+      "E", mainFont, "", "", {25, 150, 25})
+
+      title3 = myGUI.newText(skill_1.W/2 + skill_2.W*2 , hauteur - skill_1.H -20, 0, 0,
+      "T", mainFont, "", "", {25, 150, 25})
+  
     
-  panelTest1 = myGUI.newPanel(10, 220, 300, 200)
-  panelTest1:setImage(love.graphics.newImage("sprites/hud/panel1.png"))
-  panelTest1:setEvent("sprites/hud/hover", onPanelHover)
-
-  panelTest2 = myGUI.newPanel(350, 250)
-  panelTest2:setImage(love.graphics.newImage("sprites/hud/panel2.png"))
-
-  textTest = myGUI.newText(panelTest1.X+10, panelTest1.Y,
-                           300, 28, "HULL STATUS", mainFont, "", "center", {151, 220, 250})
+  G_groupTest = myGUI.newGroup()
   
-  checkBoxTest1 = myGUI.newCheckbox(250, 30, 24, 24)
-  checkBoxTest1:setImages(
-    love.graphics.newImage("sprites/hud/dotRed.png"),
-    love.graphics.newImage("sprites/hud/dotGreen.png")
-  )
-  checkBoxTest1:setEvent("pressed", onCheckboxSwitch)
+  G_groupTest:addElement(skill_3)
   
-  title1 = myGUI.newText(panelTest1.X + 35, panelTest1.Y + 45, 0, 0,
-                           "Shield Generator", mainFont, "", "", {157, 164, 174})
-  
-  progressTest1 = myGUI.newProgressBar(panelTest1.X + 35, panelTest1.Y + 68, 220, 26, 100,
-                                      {50,50,50}, {250, 129, 50})
-                                    
-  progressTest1:setImages(love.graphics.newImage("sprites/hud/progress_grey.png"),
-                         love.graphics.newImage("sprites/hud/progress_orange.png"))
+  G_groupTest:addElement(skill_2)
+  G_groupTest:addElement(skill_1)
+  G_groupTest:addElement(title1)
+  G_groupTest:addElement(title2)
+  G_groupTest:addElement(title3)
 
-  
-  title2 = myGUI.newText(panelTest1.X + 35, panelTest1.Y + 45 + 70, 0, 0,
-                           "Generator Shield", mainFont, "", "", {157, 164, 174})
-  
-  progressTest2 = myGUI.newProgressBar(panelTest1.X + 35, panelTest1.Y + 68 + 70, 220, 26, 100,
-                                      {50,50,50}, {250, 129, 50})
-                                    
-  progressTest2:setImages(love.graphics.newImage("sprites/hud/progress_grey.png"),
-                         love.graphics.newImage("sprites/hud/progress_green.png"))
-                       
-  progressTest2:setValue(0)
+  G_groupTest:addElement(inventory_slot_1)
+  G_groupTest:addElement(inventory_slot_2)
+  G_groupTest:addElement(inventory_slot_3)
+  G_groupTest:addElement(inventory_slot_4)
+  G_groupTest:addElement(inventory_slot_5)
 
-  groupTest = myGUI.newGroup()
-  groupTest:addElement(buttonYes)
-  groupTest:addElement(buttonNo)
-  groupTest:addElement(panelTest1)
-  groupTest:addElement(panelTest2)
-  groupTest:addElement(checkBoxTest1)
-  groupTest:addElement(textTest)
-  groupTest:addElement(title1)
-  groupTest:addElement(progressTest1)
-  groupTest:addElement(title2)
-  groupTest:addElement(progressTest2)
-  groupTest:addElement(buttonParam)
+  G_groupTest:addElement(buttonParam)
 
-G_groupTest = Group:new()
-    G_groupTest:addElement(G_panelTest1)
-    G_groupTest:addElement(G_healthBar)
-    G_groupTest:addElement(G_healthHeart)
-  
-
-   -- G_groupTest:addElement(G_button)
-    
-    G_checkbox = Checkbox:new(30, 30, 10, 10)
-    G_checkbox:setImages(love.graphics.newImage("sprites/hud/dotGreen.png"), love.graphics.newImage("sprites/hud/dotRed.png"))
-
-    G_groupTest = Group:new()
     G_health = Group:new()
-    G_groupTest:addElement(G_panelTest1)
-    G_groupTest:addElement(G_healthBar)
-    G_groupTest:addElement(G_checkbox)
     G_health:addElement(G_healthBar)
     G_health:addElement(G_healthHeart)
+
    
 
 end
@@ -157,11 +144,9 @@ function love.update(dt)
     G_player:update()
    
    G_groupTest:update(dt)
-   groupTest:update(dt)
 end
 
 function love.draw()
-  groupTest:draw()
   G_groupTest:draw()
     love.graphics.scale(4, 4)
     G_health:draw()
